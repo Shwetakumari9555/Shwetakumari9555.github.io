3603cc4287611f9ae7e95df57193ed91d7f3c471
sweta kumari <119477006+Shwetakumari9555@users.noreply.github.com>
2026-02-01 23:12:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,5 +73,5 @@ $(document).ready(function(){
 });
 
 function resume() {
-    window.open("https://drive.google.com/file/d/19qTmB0ytsc_SwQ5fDTfpfvjc7uFb-0Yl/view?usp=sharing");
+    window.open("https://drive.google.com/file/d/1qcWGITkIGw18bTND6TUcIhWnMlEpiOw2/view?usp=sharing");
 }
